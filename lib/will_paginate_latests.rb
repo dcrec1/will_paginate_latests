9cc42023d2e9c6a0268ffbb193412f774079f234
Diego Carrion <dcrec1@dcrec1.(none)>
2008-12-03 13:07:11 -0200

    paginate_latests now accepts a condition

--- lib/will_paginate_latests.rb
+++ lib/will_paginate_latests.rb
@@ -1,8 +1,8 @@
 ActionController::Base.class_eval do
-  def paginate_latests(sym, *map)
+  def paginate_latests(sym, map = {})
     model = sym.to_s.gsub("_", " ").singularize.split.each{ |word| word.capitalize! }.join('')
     page = params[:page] || 1
-    per_page = (map.first[:per_page] if map.size > 0) || 10
-    Kernel.const_get(model).paginate(:page => page, :order => "id DESC", :per_page => per_page)
+    map.merge!(:page => page, :order => "id DESC") 
+    Kernel.const_get(model).paginate(map)
   end
 end
--- spec/controller/will_paginate_latests_spec.rb
+++ spec/controller/will_paginate_latests_spec.rb
@@ -5,22 +5,23 @@ class RailersController < ActionController::Base
     @railers = paginate_latests(:railers)
   end
   def per_page
-    @railers = paginate_latests(:railers, :per_page => 20)
+    @railers = paginate_latests(:railers, :per_page => @@per_page)
+  end
+  def condition
+    @railers = paginate_latests(:railers, :condition => @@condition)
   end
 end
 
 describe RailersController do
   
-  before :each do
+  before :all do
     @result = 567
     @page = 1
-    @per_page = 10
   end
   
-  def expect
-    Railer.should_receive(:paginate).
-          with(:page => @page, :per_page => @per_page, :order => "id DESC").
-          and_return(@result)
+  def expect(map = {})
+    map.merge!(:page => @page, :order => "id DESC")
+    Railer.should_receive(:paginate).with(map).and_return(@result)
   end
 
   it "should paginates latests railers by id" do
@@ -31,10 +32,16 @@ describe RailersController do
   end
   
   it "should paginate given a per_page" do
-    @per_page = 20
-    expect
+    @@per_page = 20
+    expect(:per_page => @@per_page)
     get :per_page
     assigns[:railers].should eql(@result)
   end
 
+  it "should paginate given a condition" do
+    @@condition = "1 = 2"
+    expect(:condition => @@condition)
+    get :condition
+    assigns[:railers].should eql(@result)
+  end
 end
